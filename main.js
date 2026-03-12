a646ed433f5b17c964d0ebda056e09b67d5c0541
Lukas <Lukario110@outlook.de>
2026-03-12 15:29:54 +0100

    Added Hero scroll animation and time line

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
 // Get the button:
 let mybutton = document.getElementById("myBtn");
+let timeline = document.getElementById("timeline");
+console.log(timeline)
 
-// When the user scrolls down 20px from the top of the document, show the button
+// When the user scrolls down 400px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (document.body.scrollTop > 660 || document.documentElement.scrollTop > 660) {
     mybutton.style.display = "block";
+    timeline.style.display = "block";
+    timeline.classList.add("fade-in");
   } else {
     mybutton.style.display = "none";
+    timeline.style.display = "none";
   }
 }
 
@@ -17,3 +22,13 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+//Hero
+
+window.addEventListener("scroll", function () {
+  const scroll = window.scrollY;
+  const hero = document.querySelector(".hero");
+
+  hero.style.transform =
+    `translate3d(0, ${scroll / 100}%, 0) scale(${(100 - scroll / 100) / 100})`;
+});
