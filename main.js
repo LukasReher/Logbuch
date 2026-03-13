fb07c43d7835ec1238ae6db665b846bfad9aab63
Lukas <Lukario110@outlook.de>
2026-03-13 11:38:28 +0100

    Added Text and made improvements

--- main.js
+++ main.js
@@ -1,26 +1,36 @@
-// Get the button:
+// Get the button
 let mybutton = document.getElementById("myBtn");
 let timeline = document.getElementById("timeline");
-console.log(timeline)
 
-// When the user scrolls down 400px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+console.log(timeline);
+
+// When the user scrolls down, show the button
+window.onscroll = function () { scrollFunction(); };
 
 function scrollFunction() {
   if (document.body.scrollTop > 660 || document.documentElement.scrollTop > 660) {
+
     mybutton.style.display = "block";
+
     timeline.style.display = "block";
     timeline.classList.add("fade-in");
+
   } else {
+
     mybutton.style.display = "none";
     timeline.style.display = "none";
+
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
+// Scroll back to top (SMOOTH)
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+
 }
 
 //Hero
