7267b367279dbb4ad31c659c0336969e66bd7109
Lukas <Lukario110@outlook.de>
2026-03-17 15:20:24 +0100

    Worked on Calculator

---
+++ Projekte/Calculator.js
@@ -0,0 +1,39 @@
+function number1() {
+    document.getElementById("output").value += "1";
+}
+
+function number2() {
+    document.getElementById("output").value += "2";
+}
+
+function number3() {
+    document.getElementById("output").value += "3";
+}
+
+function number4() {
+    document.getElementById("output").value += "4";
+}
+
+function number5() {
+    document.getElementById("output").value += "5";
+}
+
+function number6() {
+    document.getElementById("output").value += "6";
+}
+
+function number7() {
+    document.getElementById("output").value += "7";
+}
+
+function number8() {
+    document.getElementById("output").value += "8";
+}
+
+function number9() {
+    document.getElementById("output").value += "9";
+}
+
+function number0() {
+    document.getElementById("output").value = "";
+}
